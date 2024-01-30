18b2c8c07fca786e2b7e27e483c4846874aaeb30
Danield05 <joseaquino971@gmail.com>
2024-01-29 18:45:10 -0600

    Refactor GameOver component and update styling

--- wordle-game/src/App.js
+++ wordle-game/src/App.js
@@ -1,26 +1,23 @@
-
-import { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import './App.css';
 import Board from './components/Board';
 import GameOver from './components/GameOver';
 import Keyboard from './components/Keyboard';
 import { boardwordle, generateWords } from './Words';
 
-
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardwordle);
+  const [wordSet, setWordSet] = useState(new Set());
+  const [correctWord, setCorrectWord] = useState("");
+  const [currentAttempt, setCurrAttempt] = useState({ attempt: 0, letterPoss: 0 });
+  const [gameOver, setGameOver] = useState({ gameOver: false, guessedWord: false });
 
-  const [board,setBoard] = useState(boardwordle)
-  const [wordSet,setWordSet] = useState(new Set())
-  const [correctWord,setCorrectWord] = useState("")
-  const [currentAttempt,setCurrAttempt] = useState({attempt:0,letterPoss:0})
-  const [gameOver,setGameOver] = useState({gameOver:false,guessedWord:false})
+  const [disabledLetters, setDisabledLetters] = useState([]);
+  const [almostLetters, setAlmostLetters] = useState([]);
+  const [correctLetters, setCorrectLetters] = useState([]);
 
-  const [disabledLetters,setDisabledLetters] = useState([])
-  const [almostLetters,setAlmostLetters] = useState([])
-  const [correctLetters,setCorrectLetters] = useState([])
- 
   const [successMessage, setSuccessMessage] = useState("");
   const [currentRandomWord, setCurrentRandomWord] = useState("");
 
@@ -30,7 +27,7 @@ function App() {
         const { wordSet, todaysWord } = await generateWords();
         setWordSet(wordSet);
         setCorrectWord(todaysWord);
-        setCurrentRandomWord(todaysWord); // Guardar la palabra aleatoria actual
+        setCurrentRandomWord(todaysWord);
       } catch (error) {
         console.error("Error fetching words:", error);
       }
@@ -39,88 +36,117 @@ function App() {
     fetchData();
   }, []);
 
-  //console.log(correctWord)
+  const onSelectLetter = (keyVal) => {
+    if (currentAttempt.letterPoss > 4) return;
 
-  const onSelectLetter=(keyVal)=>{
-    if(currentAttempt.letterPoss > 4) return;
-
-    const newBoard = [...board]
-    newBoard[currentAttempt.attempt][currentAttempt.letterPoss] = keyVal
-    setBoard(newBoard)
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPoss] = keyVal;
+    setBoard(newBoard);
     setCurrAttempt({
-      attempt:currentAttempt.attempt,
-      letterPoss:currentAttempt.letterPoss+1
-    })
-  }
+      attempt: currentAttempt.attempt,
+      letterPoss: currentAttempt.letterPoss + 1,
+    });
+  };
 
   const onEnter = () => {
     if (currentAttempt.letterPoss !== 5) return;
-  
+
     let currWord = "";
     for (let i = 0; i < 5; i++) {
       currWord += board[currentAttempt.attempt][i];
     }
-  
+
     setCurrAttempt({
       attempt: currentAttempt.attempt + 1,
       letterPoss: 0,
     });
-  
-    if (!wordSet.has(currWord.toLowerCase())) {
+
+    if (!wordSet.has(currWord.toUpperCase())) {
       alert("Word not in the list");
     }
-  
-    if (currWord.toLowerCase() === correctWord) {
-      setSuccessMessage("Congratulations, You won!");
+
+    if (currWord.toUpperCase() === correctWord.toUpperCase()) {
+      setSuccessMessage(`Congratulations, You won!`);
       setGameOver({ gameOver: true, guessedWord: true });
     }
-  
+
     if (currentAttempt.attempt === 5) {
-      alert("Game Over");
-      setGameOver({ gameOver: true, guessedWord: true });
+      setSuccessMessage(`You didn't guess correctly!`);
+      setGameOver({ gameOver: true, guessedWord: false });
       return;
     }
   };
 
-  const onDelete = ()=>{
-    if(currentAttempt.letterPoss === 0) return;
+  const onDelete = () => {
+    if (currentAttempt.letterPoss === 0) return;
 
-    const newBoard = [...board]
-    newBoard[currentAttempt.attempt][currentAttempt.letterPoss-1] = ""
-    setBoard(newBoard)
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPoss - 1] = "";
+    setBoard(newBoard);
     setCurrAttempt({
       ...currentAttempt,
-      letterPoss:currentAttempt.letterPoss-1
-    })
+      letterPoss: currentAttempt.letterPoss - 1,
+    });
+  };
 
-  }
+  const handlePlayAgain = () => {
+    window.location.reload();
+  };
 
   return (
     <div className="App">
-      <nav><h1>Wordle</h1></nav>
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
 
       {currentRandomWord && (
         <div className="random-word">{`${currentRandomWord}`}</div>
       )}
 
-      {successMessage && <div className="success-message">{successMessage}</div>}
-     
-      
-      <AppContext.Provider value={{board,setBoard,correctWord,wordSet,
-        setCorrectWord,currentAttempt,setCurrAttempt,gameOver,setGameOver,
-        disabledLetters,setDisabledLetters,almostLetters,setAlmostLetters,correctLetters,setCorrectLetters,
-        onSelectLetter,onEnter,onDelete
-        }}>
-      <div className='game'>
-        <Board/>
+      {successMessage && (
+        <div className="success-message">{successMessage}</div>
+      )}
 
-        {gameOver.gameOver ? <GameOver/> :  <Keyboard/>}
-       
-      </div>
-
+      <AppContext.Provider
+        value={{
+          board,
+          setBoard,
+          correctWord,
+          wordSet,
+          setCorrectWord,
+          currentAttempt,
+          setCurrAttempt,
+          gameOver,
+          setGameOver,
+          disabledLetters,
+          setDisabledLetters,
+          almostLetters,
+          setAlmostLetters,
+          correctLetters,
+          setCorrectLetters,
+          onSelectLetter,
+          onEnter,
+          onDelete,
+        }}
+      >
+        
+        <div className="game">
+          <Board />
+          {gameOver.gameOver ? <GameOver /> : <Keyboard />}
+        </div>
+        
       </AppContext.Provider>
-     
-      <br/><br/>
+        <div className='rules'>
+          <p><strong>Instrucciones:</strong> Solo tienes 6 intentos para comprobar la palabra, las casillas marcadas en 🟩 significan que tienen la letra </p>
+          <p> en la posicion correcta, y las letras marcadas en 🟨 son aquellas que pertenecen a la palabra</p>
+          <p>pero que no estan en el orden correcto.</p>
+        </div>
+      {gameOver.gameOver && (
+        <button className='newGame' onClick={handlePlayAgain}>Play Again</button>
+      )}
+
+      <br />
+      <br />
     </div>
   );
 }
